d3b2cbc65c3d4551e6e5cf8b3e292a6ffc8905ec
ayakase <wibu1892001@gmail.com>
2024-08-04 07:02:07 +0700

    add endpoint

--- constants/defaultEndpoint.ts
+++ constants/defaultEndpoint.ts
@@ -1,11 +1,35 @@
-export const BASE_URL = '.....'
+export const BASE_URL = '.....';
 
-// public routes
+// Public routes
+export const REGISTER = '/auth/register';
+export const LOGIN = '/auth/login';
+export const GET_RECENT_POSTS = '/posts/all';
+export const GET_POST_DETAILS = '/post';
+export const SEARCH_POSTS = '/search/posts';
+export const SEARCH_TAGS = '/search/tags';
+export const SEARCH_CHARACTERS = '/search/characters';
+export const SEARCH_GENRES = '/search/genres';
+export const SEARCH_CATEGORIES = '/search/categories';
+export const GET_TAGS = '/tags/all';
+export const GET_CATEGORIES = '/categories/all';
+export const GET_GENRES = '/genres/all';
 
-export const GET_RECENT_POSTS = '/posts/all'
-export const GET_POST_DETAILS = '/post'
+// Private routes
+export const GET_USER_INFO = '/user/info';
+export const GET_USER_LIKED_POSTS = '/user/liked';
+export const CREATE_POST = '/posts/create';
+export const UPDATE_POST = '/posts/update';
+export const DELETE_POST = '/posts/delete';
+export const LIKE_POST = '/posts/like';
+export const UNLIKE_POST = '/posts/unlike';
+export const ADD_COMMENT = '/posts/comment/add';
+export const DELETE_COMMENT = '/posts/comment/delete';
 
-// private routes
-
-export const GET_USER_INFO = '/user/info'
-export const GET_USER_LIKED_POSTS = '/user/liked'
+// Extra endpoints for better functionality
+export const GET_USER_POSTS = '/user/posts';
+export const GET_USER_COMMENTS = '/user/comments';
+export const UPDATE_USER_PROFILE = '/user/update';
+export const FOLLOW_USER = '/user/follow';
+export const UNFOLLOW_USER = '/user/unfollow';
+export const GET_FOLLOWERS = '/user/followers';
+export const GET_FOLLOWING = '/user/following';
--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-11/12 m-auto mt-10">
+    <div class="w-10/12 m-auto mt-10">
         <div class="flex flex-row flex-wrap gap-2">
             <div v-for="tag in tags"
-                class="center relative inline-block select-none whitespace-nowrap rounded-lg bg-rose-700 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                class="center relative inline-block select-none whitespace-nowrap rounded-lg bg-rose-700 py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white cursor-pointer">
                 <div class="mt-px">#{{ tag }}</div>
             </div>
         </div>
@@ -18,24 +18,6 @@ const tags = [
     "Fantasy",
     "Horror",
     "Mystery",
-    "Psychological",
-    "Romance",
-    "Sci-Fi",
-    "Slice of Life",
-    "Sports",
-    "Supernatural",
-    "Thriller",
-    "Ecchi",
-    "Harem",
-    "Isekai",
-    "Mecha",
-    "Music",
-    "Shounen",
-    "Shoujo",
-    "Josei",
-    "Seinen",
-    "Yaoi",
-    "Yuri",
     "Dementia",
     "Demons",
     "Historical",
